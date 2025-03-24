de0199526dc75fef07e61061aec018161a9e39e0
PhuongBT337 <b.t.phuong337@gmail.com>
2025-03-25 01:37:24 +0900

    update bai tap 02

---
+++ Ex02/Ex02.js
@@ -0,0 +1,7 @@
+let resultString= prompt('What is the “official” name of JavaScript?');
+
+if(resultString === 'ECMAScript'){
+    alert('Right!');
+}else{
+    alert('Didn`t know? ECMAScript!');
+}
