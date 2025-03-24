7555ece829270503011af42371c93044eaf152a7
PhuongBT337 <b.t.phuong337@gmail.com>
2025-03-25 01:45:14 +0900

    update bai tap 03

---
+++ Ex03/Ex03.js
@@ -0,0 +1,37 @@
+let readNumber = Number(prompt('Hay nhap so 0-9: '));
+
+switch (readNumber) {
+    case 0:
+        alert('So khong');
+        break;
+    case 1:
+        alert('So mot');
+        break;
+    case 2:
+        alert('So hai');
+        break;
+    case 3:
+        alert('So ba');
+        break;
+    case 4:
+        alert('So bon');
+        break;
+    case 5:
+        alert('So nam');
+        break;
+    case 6:
+        alert('So sau');
+        break;
+    case 7:
+        alert('So bay');
+        break;
+    case 8:
+        alert('So tam');
+        break;
+    case 9:
+        alert('So chin');
+        break;
+    default:
+        alert('Xin hay nhap so 0-9');
+        break;
+}
